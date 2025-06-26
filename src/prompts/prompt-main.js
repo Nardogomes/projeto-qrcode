485b974ed51eb42618e93da8ae787fb202962d97
Ednardo Gomes Dos Santos <ednardo.santos@lanlink.com.br>
2025-06-26 11:58:56 -0300

    feat: configure the Chalk library.

--- src/prompts/prompt-main.js
+++ src/prompts/prompt-main.js
@@ -1,9 +1,11 @@
+import chalk from "chalk"
+
 export const mainPrompt = [
   {
     name: "select",
-    description: "Escolha a ferramenta (1 - Gerar QrCode ou 2 - Gerar senha)",
+    description: chalk.yellow.bold("Escolha a ferramenta (1 - Gerar QrCode ou 2 - Gerar senha)"),
     pattern: /^[1-2]+$/,
-    message: "Escolha apenas entre 1 e 2",
+    message: chalk.bgRed.bold("Escolha apenas entre 1 e 2"),
     required: true
   }
 ]
